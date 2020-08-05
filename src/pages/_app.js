d25aa2741c852e92b1fb5c8f6bed630b6db0dae0
alexalikiotis <alexalikiotis5@gmail.com>
2020-08-05 19:14:52 +0300

    setting up custom babel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/main.css';
+import 'src/styles/main.css';
 
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 const IndexPage = () => {
-  return <h1>Next.js app</h1>;
+  return <h1 className="text-3xl ml-2">Next.js app</h1>;
 };
 
 export default IndexPage;
