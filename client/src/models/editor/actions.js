c1d2596aac041a628c827ef8fa754396766b841f
alexalikiotis <alexalikiotis5@gmail.com>
2020-08-05 18:55:13 +0300

    clean slate

--- client/src/models/editor/actions.js
+++
@@ -1,6 +0,0 @@
-export const UPDATE_EDITOR = 'app/UPDATE_EDITOR';
-
-export const updateEditor = payload => ({
-  type: UPDATE_EDITOR,
-  payload,
-});
--- client/src/models/settings/actions.js
+++
@@ -1,6 +0,0 @@
-export const UPDATE_SETTINGS = 'app/UPDATE_SETTINGS';
-
-export const updateSettings = payload => ({
-  type: UPDATE_SETTINGS,
-  payload,
-});
