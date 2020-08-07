14551a003c691e6f9056c3275a4a15fa3d3502c0
alexalikiotis <alexalikiotis5@gmail.com>
2020-08-07 22:25:23 +0300

    point preview to old backend

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -11,7 +11,7 @@ const CSS = {
 };
 
 const fetcher = blueprint => {
-  return fetch('http://localhost:3000/api/preview', {
+  return fetch('https://blueprint-dash-server.herokuapp.com/blueprint', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
